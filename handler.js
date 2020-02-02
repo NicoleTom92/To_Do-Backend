8743f10f6fa48a123fec64c3c2e09cebdab817b8
Nicole <nicole.tomlinson92@gmail.com>
2020-02-02 21:32:24 +0000

    updated code

--- handler.js
+++ handler.js
@@ -12,12 +12,13 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'TODOS'
+  database: "TODOS"
 });
 
 //GET//tasks
 app.get("/tasks", function (request, response) {
-  connection.query("SELECT * FROM tasks", function (err, data) {
+  console.log("received request for get/Tasks");
+  connection.query("SELECT * FROM Task", function (err, data) {
     if (err) {
       response.status(500).json({
         error: err
@@ -32,8 +33,9 @@ app.get("/tasks", function (request, response) {
 
 //POST//tasks
 app.post("/tasks", function (request, response) {
+  console.log("received request for post/tasks", { data: request.body})
   const newTasks = request.body;
-  connection.query("INSERT INTO tasks SET ?", [newTasks], function (err, data) {
+  connection.query("INSERT INTO Task SET ?", [newTasks], function (err, data) {
     if (err) {
       response.status(500).json({error: err});
     } else {
@@ -47,7 +49,10 @@ app.post("/tasks", function (request, response) {
 app.put("/tasks/:id", function (request, response) {
   const updatedTasks = request.body;
   const id = request.params.id;
-  connection.query('UPDATE Tasks SET ? WHERE id= ?' , [updatedTasks, id],
+  console.log(`received request for put/tasks/${id}` , {
+    data:updatedTasks
+  });
+  connection.query('UPDATE Task SET ? WHERE id= ?' , [updatedTasks, id],
     function (err) {
       if (err) {
         response.status(500).json({ error: err });
@@ -60,8 +65,8 @@ app.put("/tasks/:id", function (request, response) {
 
 //DELETE//
 app.delete("/tasks/:id", function (request, response) {
-  const id2 = request.params.id;
-  connection.query("DELETE FROM Task WHERE id=?", [id2], function (err) {
+  const id1 = request.params.id;
+  connection.query("DELETE FROM Task WHERE id=?", [id1], function (err) {
     if (err) {
       response.status(500).json({
         error: err
